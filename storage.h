a0e10ea20c1e85ac2122e05349bf85d74e65714f
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2015-02-22 16:26:14 -0500

    Removed murmurhash and implemended FNV-1 as murmurhash was generating too many collisions.

--- storage.h
+++ storage.h
@@ -8,9 +8,21 @@
 #include <sys/stat.h>
 #include <sqlite3.h>
 #include <bstrlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #include "util.h"
-#include "murmurhash.h"
+
+/* #define STORAGE_HASH_MURMUR 1 */
+#define STORAGE_HASH_FNV 1
+
+#ifdef STORAGE_HASH_MURMUR
+#define STORAGE_HASH_BUFFER_SIZE 4
+#define STORAGE_HASH_SEED 32
+#endif /* STORAGE_HASH_MURMUR */
+
+#ifdef STORAGE_HASH_FNV
+#endif /* STORAGE_HASH_FNV */
 
 typedef struct {
    bstring path;
@@ -25,6 +37,12 @@ typedef struct {
 int storage_ensure_database( bstring );
 int storage_inventory_update_file( sqlite3*, bstring );
 int storage_inventory_update_walk( bstring, bstring );
+#ifdef STORAGE_HASH_MURMUR
+uint32_t storage_hash_file( bstring );
+#endif /* STORAGE_HASH_MURMUR */
+#ifdef STORAGE_HASH_FNV
+uint64_t storage_hash_file( bstring );
+#endif /* STORAGE_HASH_FNV */
 int storage_sql_storage_file( sqlite3_stmt*, storage_file* );
 void storage_free_storage_file( storage_file* );
 
