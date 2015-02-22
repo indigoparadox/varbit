6a7496bb53ffb88fe8e8081c78adfb83e9fb6947
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2015-02-22 10:20:02 -0500

    Added superficial inode/date insertion to DB.

--- murmurhash.h
+++ murmurhash.h
@@ -2,5 +2,9 @@
 #ifndef MURMURHASH_H
 #define MURMURHASH_H
 
+#include <stdint.h>
+
+uint32_t murmur3_32(const char *, uint32_t, uint32_t);
+
 #endif /* MURMURHASH_H */
 
--- util.h
+++ util.h
@@ -4,10 +4,24 @@
 
 #define DBG_ERR( ... ) fprintf( stderr, __VA_ARGS__ )
 
-#define CATCH( retval, val_arg, ... ) \
-   if( retval ) { \
+#define CATCH_NONZERO( local_retval, proc_retval, val_arg, ... ) \
+   if( local_retval ) { \
       DBG_ERR( __VA_ARGS__ ); \
-      retval = val_arg; \
+      proc_retval = val_arg; \
+      goto cleanup; \
+   }
+
+#define CATCH_NULL( local_retval, proc_retval, val_arg, ... ) \
+   if( NULL == local_retval ) { \
+      DBG_ERR( __VA_ARGS__ ); \
+      proc_retval = val_arg; \
+      goto cleanup; \
+   }
+
+#define CATCH_BSTRERR( local_retval, proc_retval, val_arg ) \
+   if( BSTR_ERR == local_retval ) { \
+      DBG_ERR( "Unable to assign bstring. Aborting.\n" ); \
+      proc_retval = val_arg; \
       goto cleanup; \
    }
 
